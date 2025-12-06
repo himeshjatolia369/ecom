7c887a1066cfed837b8adf504eac665797c2c2d5
himeshjatolia369 <himesh.jatolia.5100781@ves.ac.in>
2025-12-06 13:13:19 +0530

    basic schema with folder structure is done

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app=express();
+
+const path=require('path');
+const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
+
+app.set("view engine","ejs")
+
+app.get("/",(req,res)=>{
+    res.send("hi")
+})
+
+app.listen(3000);
