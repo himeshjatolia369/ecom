2303fcfb3aca2f95a82a0f983891639b45b0b9d4
himeshjatolia369 <himesh.jatolia.5100781@ves.ac.in>
2025-12-06 20:57:18 +0530

    added seperation of concern in module and created owner module and learn router and tested it in postman

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express=require('express');
 const app=express();
-
+const userModule=require("./models/user-module")
+const productModule=require("./models/product-module")
+const ownerModule=require("./models/owner-module")
 const path=require('path');
 const cookieParser = require('cookie-parser');
+
+const db=require("./config/mongoose-connection");
+const userRoute = require('./routes/usersRouter');
+const productRoute=require('./routes/productsRouter');
+const ownerRoute=require('./routes/ownersRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 
+
 app.set("view engine","ejs")
 
-app.get("/",(req,res)=>{
-    res.send("hi")
-})
+app.use("/users",userRoute)
+app.use("/products",productRoute)
+app.use("/owner",ownerRoute)
 
 app.listen(3000);
